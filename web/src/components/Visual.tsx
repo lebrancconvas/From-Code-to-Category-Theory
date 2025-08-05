36a64fb6441799dbcac073e36737c910ac19f999
lebrancconvas <lebrancconvas@gmail.com>
2025-08-05 19:04:42 +0700

    ✨ feat; get type signature when writing the typescript code for function declaration.

--- web/src/components/Visual.tsx
+++ web/src/components/Visual.tsx
@@ -1,9 +1,11 @@
+import { getFunctionSignature } from "@/utils/functionSignature";
+
 interface VisualProps {
   code: string;
 };
 
 export function Visual({ code }: VisualProps) {
   return (
-    <h1>{ code }</h1>
+    <h1>{ getFunctionSignature(code) }</h1>
   )
 };
