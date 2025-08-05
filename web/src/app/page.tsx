c5bfa5756da0f8dcea1a2e75e453d1cb94610fcc
lebrancconvas <lebrancconvas@gmail.com>
2025-08-05 09:22:14 +0700

    💄 style: divide visual and editor into grid section.

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -4,12 +4,14 @@ import { Visual } from "@/components/Visual"
 export default function App() {
   return (
     <>
-      <section className="A-Side">
-        <Visual />
-      </section>
-      <section className="B-Side">
-        <Editor />
-      </section>
+      <div className="panel">
+        <section className="A-Side">
+          <Visual />
+        </section>
+        <section className="B-Side">
+          <Editor />
+        </section>
+      </div>
     </>
   )
 };
