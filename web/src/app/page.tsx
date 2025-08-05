9ce2fa912ca600cb0edd41349cb779c22e143074
lebrancconvas <lebrancconvas@gmail.com>
2025-08-05 14:14:40 +0700

    💄 style: styling a panel and ✨ feat: create footer.

--- web/src/app/page.tsx
+++ web/src/app/page.tsx
@@ -1,16 +1,20 @@
 import { Editor } from "@/components/Editor"
 import { Visual } from "@/components/Visual"
+import { Footer } from "@/components/Footer"
 
 export default function App() {
   return (
     <>
       <div className="panel">
-        <section className="A-Side">
+        <section className="visual-section">
           <Visual />
         </section>
-        <section className="B-Side">
+        <section className="editor-section">
           <Editor />
         </section>
+        <footer>
+          <Footer />
+        </footer>
       </div>
     </>
   )
